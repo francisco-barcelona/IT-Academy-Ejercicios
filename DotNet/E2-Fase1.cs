d7e629410a36b06421ff1811b1005a069cce7c35
Francisco Javier Rivas Martín <60388992+francisco-barcelona@users.noreply.github.com>
2020-04-07 12:03:51 +0200

    Create E2-Fase1.cs

---
+++ DotNet/E2-Fase1.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace E2_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nom = "Paco";
+            String cognom1 = "Rivas";
+            String cognom2 = "Martín";
+            int dia = 7;
+            int mes = 4;
+            int any = 2020;
+
+            Console.WriteLine(cognom1 + " " + cognom2 + " " + nom);
+            Console.WriteLine(dia + "/" + mes + "/" + any);
+        }
+    }
+}
