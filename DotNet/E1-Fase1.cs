150cb0532b68e975316a0e954cbfc548815b9779
Francisco Javier Rivas Martín <60388992+francisco-barcelona@users.noreply.github.com>
2020-04-07 10:57:04 +0200

    Create E1-Fase1.cs

---
+++ DotNet/E1-Fase1.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Ejercicio1Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] nombre = { 'P', 'a', 'c', 'o' };
+
+            foreach (var value in nombre)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
