fbd8a6d380639ae97be478aca36815d39eb06f24
Francisco Javier Rivas Martín <60388992+francisco-barcelona@users.noreply.github.com>
2020-04-14 11:58:01 +0200

    Fase 3
    Fase 3

---
+++ DotNet/Ejercicio-Ciudades/Fase3/Ciudades/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Ciudades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String var1;
+            String var2;
+            String var3;
+            String var4;
+            String var5;
+            String var6;
+            var arrayCiutats = new String[6];
+            var ArrayCiutatsModificades = new String[6];
+            String modVar;
+
+            Console.WriteLine("Escribe el nombre de la ciudad 1");
+            var1 = Console.ReadLine();
+            Console.WriteLine("Escribe el nombre de la ciudad 2");
+            var2 = Console.ReadLine();
+            Console.WriteLine("Escribe el nombre de la ciudad 3");
+            var3 = Console.ReadLine();
+            Console.WriteLine("Escribe el nombre de la ciudad 4");
+            var4 = Console.ReadLine();
+            Console.WriteLine("Escribe el nombre de la ciudad 5");
+            var5 = Console.ReadLine();
+            Console.WriteLine("Escribe el nombre de la ciudad 6");
+            var6 = Console.ReadLine();
+
+            arrayCiutats[0] = var1;
+            arrayCiutats[1] = var2;
+            arrayCiutats[2] = var3;
+            arrayCiutats[3] = var4;
+            arrayCiutats[4] = var5;
+            arrayCiutats[5] = var6;
+
+            for (int i = 0; i < arrayCiutats.Length; i++)
+            {
+                modVar = arrayCiutats[i].Replace("a", "4");
+                ArrayCiutatsModificades[i] = modVar;
+            }
+
+            Array.Sort(ArrayCiutatsModificades);
+
+            for (int i = 0; i < ArrayCiutatsModificades.Length; i++)
+            {
+                Console.WriteLine(ArrayCiutatsModificades[i]);
+            }
+        }
+    }
+}
