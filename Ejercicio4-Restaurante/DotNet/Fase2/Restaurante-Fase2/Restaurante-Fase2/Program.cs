3c004da82cbe38bf7fe92f1e5db1be6fd45a1bf6
francisco-barcelona <rfrancisco31@hotmail.com>
2020-04-20 12:57:44 +0200

    Eliminación variables innecesarias

--- Ejercicio4-Restaurante/DotNet/Fase2/Restaurante-Fase2/Restaurante-Fase2/Program.cs
+++ Ejercicio4-Restaurante/DotNet/Fase2/Restaurante-Fase2/Restaurante-Fase2/Program.cs
@@ -7,14 +7,6 @@ namespace Restaurante_Fase2
     {
         static void Main(string[] args)
         {
-            int bitllet5 = 5;
-            int bitllet10 = 10;
-            int bitllet20 = 20;
-            int bitllet50 = 50;
-            int bitllet100 = 100;
-            int bitllet200 = 200;
-            int bitllet500 = 500;
-            int preuTotal;
             var menu = new String[5];
             var preuPlat = new int[5];
             List<String> comanda = new List<String>();
