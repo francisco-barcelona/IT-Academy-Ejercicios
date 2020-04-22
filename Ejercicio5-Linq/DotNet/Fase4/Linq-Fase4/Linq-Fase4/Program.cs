a93433ffeac61f529b7c5f0eb29e4d25296dfd62
francisco-barcelona <rfrancisco31@hotmail.com>
2020-04-22 12:53:18 +0200

    Ejercicio Linq - Fase 4

---
+++ Ejercicio5-Linq/DotNet/Fase4/Linq-Fase4/Linq-Fase4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Linq_Fase4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] listaNombres = {"David", "Sergio", "Maria", "Laura", "Oscar", "Julia", "Oriol"};
+            IEnumerable<String> nombresEmpiezanO = from n in listaNombres
+                                                   where n.Contains("O")
+                                                   select n;
+            IEnumerable<String> nombresLargos6 = from n in listaNombres
+                                                 where n.Length > 6
+                                                 select n;
+            IEnumerable<String> nombresOrdenados = from n in listaNombres
+                                                   orderby n descending
+                                                   select n;
+
+            Console.WriteLine("Noms que comencen amb O: ");
+            foreach (String nombre in nombresEmpiezanO){
+                Console.WriteLine(nombre);
+            }
+            Console.WriteLine("Noms amb més de 6 caràcters: ");
+            foreach (String nombre in nombresLargos6)
+            {
+                Console.WriteLine(nombre);
+            }
+            Console.WriteLine("Noms ordenats: ");
+            foreach(String nombre in nombresOrdenados)
+            {
+                Console.WriteLine(nombre);
+            }
+        }
+    }
+}
