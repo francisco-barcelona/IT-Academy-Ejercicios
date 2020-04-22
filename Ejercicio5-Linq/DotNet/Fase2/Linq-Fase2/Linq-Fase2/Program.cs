ffad6c422c1d3b251740c2019c7fc9b1b36af662
francisco-barcelona <rfrancisco31@hotmail.com>
2020-04-22 10:53:00 +0200

    Ejercicio Linq - Fase2

---
+++ Ejercicio5-Linq/DotNet/Fase2/Linq-Fase2/Linq-Fase2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Linq_Fase2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] listaNumeros = { 2, 6, 8, 4, 5, 5, 9, 2, 1, 8, 7, 5, 9, 6, 4 };
+            double mediaListaNumeros = listaNumeros.Average();
+            int maxListaNumeros = listaNumeros.Max();
+            int minListaNumeros = listaNumeros.Min();
+
+            Console.WriteLine($"Media de la lista: {mediaListaNumeros}. Máximo de la lista: {maxListaNumeros}. Mínimo de la lista: {minListaNumeros}");
+        }
+    }
+}
