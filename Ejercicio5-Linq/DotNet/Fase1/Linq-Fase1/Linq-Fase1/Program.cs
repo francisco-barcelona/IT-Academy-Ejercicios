e9265ff50fe1bc6933e8827bc8fa01bf3b7ccac3
francisco-barcelona <rfrancisco31@hotmail.com>
2020-04-22 10:26:19 +0200

    Ejercicio Linq - Fase1

---
+++ Ejercicio5-Linq/DotNet/Fase1/Linq-Fase1/Linq-Fase1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Linq_Fase1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] listaNumeros = {2, 6, 8, 4, 5, 5, 9, 2, 1, 8, 7, 5, 9, 6, 4};
+            IEnumerable<int> numeros = from n in listaNumeros
+                                       where n % 2 == 0
+                                       select n;
+            foreach(int numero in numeros)
+            {
+                Console.WriteLine(numero);
+            }
+
+        }
+    }
+}
