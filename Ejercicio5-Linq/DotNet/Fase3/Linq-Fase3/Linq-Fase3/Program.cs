04efd3037df61c273c3945ed2f3afa5b14b8f433
francisco-barcelona <rfrancisco31@hotmail.com>
2020-05-04 17:34:17 +0200

    Ejercicios con sintaxis Fluent

--- Ejercicio5-Linq/DotNet/Fase3/Linq-Fase3/Linq-Fase3/Program.cs
+++ Ejercicio5-Linq/DotNet/Fase3/Linq-Fase3/Linq-Fase3/Program.cs
@@ -8,22 +8,17 @@ namespace Linq_Fase3
     {
         static void Main(string[] args)
         {
-            int[] listaNumeros = { 2, 6, 8, 4, 5, 5, 9, 2, 1, 8, 7, 5, 9, 6, 4 }; 
-            IEnumerable<int> numerosMesGrans5 = from n in listaNumeros
-                                       where n>5
-                                       select n;
-
-            IEnumerable<int> numerosMesPetits5 = from n in listaNumeros
-                                                where n < 5
-                                                select n;
+            IList<int> listaNumeros = new List<int>() { 2, 6, 8, 4, 5, 5, 9, 2, 1, 8, 7, 5, 9, 6, 4 };
+            var numeros = listaNumeros.Where(s => s > 5).ToList<int>();
+            var numeros2 = listaNumeros.Where(s => s < 5).ToList<int>();
 
             Console.WriteLine("Números més grans que 5");
-            foreach (int numero in numerosMesGrans5)
+            foreach (int numero in numeros)
             {
                 Console.WriteLine(numero);
             }
             Console.WriteLine("Números més petits que 5");
-            foreach (int numero in numerosMesPetits5)
+            foreach (int numero in numeros2)
             {
                 Console.WriteLine(numero);
             }
