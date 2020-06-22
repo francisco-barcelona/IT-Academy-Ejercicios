545fbdfc8c907998c8719f513a56d72f14653567
francisco-barcelona <rfrancisco31@hotmail.com>
2020-06-22 11:15:44 +0200

    Add menu: insert student&marks, student's name and average marks

--- Prácticas/ConsoleApp/ConsoleApp1/Program.cs
+++ Prácticas/ConsoleApp/ConsoleApp1/Program.cs
@@ -6,6 +6,7 @@ namespace ConsoleApp1
     class Program
     {
         static string EscapeWord = "exit";
+        static string option;
 
         static void Main(string[] args)
         {
@@ -18,54 +19,75 @@ namespace ConsoleApp1
 
             while (keepDoing)
             {
-                Console.WriteLine($"Nombre del alumno {nombresDeAlumnos.Count + 1}:");
-                var nombreText = Console.ReadLine();
-                if (nombreText == EscapeWord)
+                menu();
+                option = Console.ReadLine();
+                if(option == "i")
                 {
-                    keepDoing = false;
-                } else
-                {
-                    Console.WriteLine($"Nota del alumno {notasDeAlumnos.Count + 1}:");
-                    var notaText = Console.ReadLine();
-
-                    if (notaText == EscapeWord)
+                    Console.WriteLine($"Nombre del alumno {nombresDeAlumnos.Count + 1}:");
+                    var nombreText = Console.ReadLine();
+                    if (nombreText == EscapeWord)
                     {
                         keepDoing = false;
                     }
                     else
                     {
-                        nombresDeAlumnos.Add(nombreText);
+                        Console.WriteLine($"Nota del alumno {notasDeAlumnos.Count + 1}:");
+                        var notaText = Console.ReadLine();
 
-                        var nota = 0.0;
-
-                        if (double.TryParse(notaText.Replace(".", ","), out nota))
+                        if (notaText == EscapeWord)
                         {
-                            notasDeAlumnos.Add(nota);
+                            keepDoing = false;
                         }
                         else
                         {
-                            Console.WriteLine("La nota introducida es incorrecta melón!");
+                            nombresDeAlumnos.Add(nombreText);
+
+                            var nota = 0.0;
+
+                            if (double.TryParse(notaText.Replace(".", ","), out nota))
+                            {
+                                notasDeAlumnos.Add(nota);
+                            }
+                            else
+                            {
+                                Console.WriteLine("La nota introducida es incorrecta melón!");
+
+                            }
                         }
                     }
+                } else if(option == "s")
+                {
+                    Console.WriteLine("Nombres de los alumnos: ");
+
+                    for (var i = 0; i < nombresDeAlumnos.Count; i++)
+                    {
+                        Console.WriteLine(nombresDeAlumnos[i]);
+                    }
+                } else if(option == "m")
+                {
+                    var suma = 0.0;
+
+                    for (var i = 0; i < notasDeAlumnos.Count; i++)
+                    {
+                        suma += notasDeAlumnos[i];
+                    }
+
+                    var average = suma / notasDeAlumnos.Count;
+                    Console.WriteLine("la media los exámenes es: {0}", average);
+                } else
+                {
+                    Console.WriteLine("La opción escogida no es válida, vuelva a intentarlo.");
+                    menu();
                 }                
-            }
+            } 
 
-            Console.WriteLine("Nombres de los alumnos: ");
-
-            for(var i = 0; i < nombresDeAlumnos.Count; i++)
+            static void menu()
             {
-                Console.WriteLine(nombresDeAlumnos[i]);
-            }
-
-            var suma = 0.0;
-
-            for (var i = 0; i < notasDeAlumnos.Count; i++)
-            {
-                suma += notasDeAlumnos[i];
-            }
-
-            var average = suma / notasDeAlumnos.Count;
-            Console.WriteLine("la media los exámenes es: {0}", average);
+                Console.WriteLine("Menú de opciones de la escuela:");
+                Console.WriteLine("Pulsa 'i' para insertar notas de exámenes.");
+                Console.WriteLine("Pulsa 's' para ver nombres de alumnos");
+                Console.WriteLine("Pulsa 'm' para ver la media de las notas de los alumnos");
+            }            
         }
     }
 }
